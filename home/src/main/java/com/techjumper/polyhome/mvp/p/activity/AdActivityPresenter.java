ded81e5bc0f2e221126b4b7482b7d1c2cf3e6e88
KevinRoy <luozheng216@gmail.com>
2016-05-03 11:45:49 +0800

    转移dimens到commonres

---
+++ home/src/main/java/com/techjumper/polyhome/mvp/p/activity/AdActivityPresenter.java
@@ -0,0 +1,20 @@
+package com.techjumper.polyhome.mvp.p.activity;
+
+import android.os.Bundle;
+
+import com.techjumper.polyhome.mvp.v.activity.AdActivity;
+
+/**
+ * Created by kevin on 16/4/29.
+ */
+public class AdActivityPresenter extends AppBaseActivityPresenter<AdActivity> {
+    @Override
+    public void initData(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onViewInited(Bundle savedInstanceState) {
+
+    }
+}
---
+++ home/src/main/java/com/techjumper/polyhome/mvp/v/activity/AdActivity.java
@@ -0,0 +1,21 @@
+package com.techjumper.polyhome.mvp.v.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.techjumper.polyhome.mvp.p.activity.AdActivityPresenter;
+
+/**
+ * Created by kevin on 16/4/29.
+ */
+public class AdActivity extends AppBaseActivity<AdActivityPresenter> {
+    @Override
+    protected View inflateView(Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+}
