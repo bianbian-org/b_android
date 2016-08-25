4964b752397229ce1df715c63a6cc11514530327
KevinRoy <luozheng216@gmail.com>
2016-08-25 14:35:39 +0800

    更新url管理

--- commonres/src/main/java/com/techjumper/commonres/ComConstant.java
+++ commonres/src/main/java/com/techjumper/commonres/ComConstant.java
@@ -1,5 +1,7 @@
 package com.techjumper.commonres;
 
+import com.techjumper.polyhome_b.adlib.Config;
+
 /**
  * Created by kevin on 16/5/18.
  */
@@ -16,17 +18,17 @@ public class ComConstant {
     public static final String PAGESIZE = "10";
 
     //host url 的开关, true 为 debug模式, false 为 release模式
-    private static final boolean isDebug = true;
+    private static final boolean isDebug = false;
 
     //保存本地数据的文件名
     public static final String FILE_FAMILY_REGISTER = "poly_b_family_register";
     public static final String FILE_MEDICAL = "personInfor";
 
-    public static final String HOST_DEBUG = "http://pl.techjumper.com";
-    public static final String HOST_RELEASE = "http://api.ourjujia.com";
+//    public static final String HOST_DEBUG = "http://pl.techjumper.com";
+//    public static final String HOST_RELEASE = "http://api.ourjujia.com";
+//
+//    public static final String HOST = isDebug ? HOST_DEBUG : HOST_RELEASE;
 
-    public static final String HOST = isDebug ? HOST_DEBUG : HOST_RELEASE;
-
-    public static final String BASE_DEBUG_B_URL = HOST + "/api/v1b/";
-    public static final String BASE_DEBUG_URL = HOST + "/api/v1/";
+    public static final String BASE_DEBUG_B_URL = Config.sHost + "/api/v1b/";
+    public static final String BASE_DEBUG_URL = Config.sHost + "/api/v1/";
 }
