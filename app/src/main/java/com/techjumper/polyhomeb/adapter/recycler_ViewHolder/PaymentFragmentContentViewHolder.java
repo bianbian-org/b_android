5ad94c86f171df66d5fda8293ca94c6577e728e9
lixin <zhuxianlixin@gmail.com>
2016-10-14 11:08:41 +0800

    优化订单部分的日期

--- app/src/main/java/com/techjumper/polyhomeb/adapter/recycler_ViewHolder/PaymentFragmentContentViewHolder.java
+++ app/src/main/java/com/techjumper/polyhomeb/adapter/recycler_ViewHolder/PaymentFragmentContentViewHolder.java
@@ -114,7 +114,7 @@ public class PaymentFragmentContentViewHolder extends BaseRecyclerViewHolder<Pay
     }
 
     private void allPaid(PaymentFragmentContentData data) {
-        setText(R.id.tv_time, String.format(getContext().getString(R.string.pay_day), data.getTime()));
+        setText(R.id.tv_time, String.format(getContext().getString(R.string.pay_day), data.getPayment_date()));
         if (sLate == data.getIs_late()) {   //已经逾期
             String text1 = String.format(getContext().getString(R.string.late_x_day), Math.abs(data.getDay())); //超过X天
             String text2 = getContext().getString(R.string.x_exceed);  //产生
