bea3e41ff75adbea1d414bce0e7ff5013b4e37d9
KevinRoy <luozheng216@gmail.com>
2016-10-24 18:15:57 +0800

    更新字体和广告逻辑

--- adlib/src/main/java/com/techjumper/polyhome_b/adlib/window/AdWindowManager.java
+++ adlib/src/main/java/com/techjumper/polyhome_b/adlib/window/AdWindowManager.java
@@ -1,20 +1,24 @@
 package com.techjumper.polyhome_b.adlib.window;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.PixelFormat;
 import android.graphics.SurfaceTexture;
-import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.squareup.picasso.RequestCreator;
 import com.techjumper.corelib.utils.Utils;
 import com.techjumper.corelib.utils.common.JLog;
 import com.techjumper.lib2.utils.PicassoHelper;
+import com.techjumper.polyhome_b.adlib.R;
 import com.techjumper.polyhome_b.adlib.entity.AdEntity;
 import com.techjumper.polyhome_b.adlib.widget.MyTextureView;
 
@@ -26,6 +30,7 @@ public class AdWindowManager {
     private FrameLayout mContainer;
     private WindowManager.LayoutParams mContainerParams;
     private ImageView mImageView;
+    private LinearLayout callLayout;
     private MyTextureView myVideoView;
     private boolean mIsAttach;
     private IAdWindow iAdWindow;
@@ -36,6 +41,7 @@ public class AdWindowManager {
         mWindowManager = (WindowManager) Utils.appContext.getSystemService(Context.WINDOW_SERVICE);
 
         mContainer = new FrameLayout(Utils.appContext);
+        callLayout = (LinearLayout) LayoutInflater.from(Utils.appContext).inflate(R.layout.layout_sleep_ad, null);
         mContainer.setBackgroundColor(0xFF000000);
         mContainerParams = new WindowManager.LayoutParams();
         mContainerParams.type = WindowManager.LayoutParams.TYPE_PHONE;
@@ -60,6 +66,15 @@ public class AdWindowManager {
         FrameLayout.LayoutParams videoLayoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT
                 , FrameLayout.LayoutParams.MATCH_PARENT);
         mContainer.addView(myVideoView, videoLayoutParams);
+
+        mContainer.addView(callLayout);
+        callLayout.setOnClickListener(v -> {
+            Intent it = new Intent();
+            ComponentName componentName = new ComponentName("com.dnake.talk", "com.dnake.activity.TalkingActivity");
+            it.setComponent(componentName);
+            it.putExtra("com.dnake.talk", "CallingActivity");
+            Utils.appContext.startActivity(it);
+        });
     }
 
     public void setOnAdsClickListener(ParentClickListener listener) {
