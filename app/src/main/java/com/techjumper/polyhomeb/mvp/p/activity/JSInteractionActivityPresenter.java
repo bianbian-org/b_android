2042595af2cdd1ae3dcfe0c4ce399ded3a037120
lixin <zhuxianlixin@gmail.com>
2016-11-02 22:28:13 +0800

    youhua

--- app/src/main/java/com/techjumper/polyhomeb/mvp/p/activity/JSInteractionActivityPresenter.java
+++ app/src/main/java/com/techjumper/polyhomeb/mvp/p/activity/JSInteractionActivityPresenter.java
@@ -143,6 +143,8 @@ public class JSInteractionActivityPresenter extends AppBaseActivityPresenter<JSI
         if (TextUtils.isEmpty(mArticleId)) return;
         DialogUtils.getBuilder(getView())
                 .content(R.string.confirm_delete_article)
+                .positiveText(R.string.ok)
+                .negativeText(R.string.cancel)
                 .onAny((dialog, which) -> {
                     switch (which) {
                         case POSITIVE:
