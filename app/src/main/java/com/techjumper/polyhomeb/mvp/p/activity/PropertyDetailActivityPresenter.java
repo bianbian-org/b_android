0183e38ea6b571ed26d9536bb9aa9518b2796d4a
lixin <zhuxianlixin@gmail.com>
2016-11-04 11:56:10 +0800

    解决bug

--- app/src/main/java/com/techjumper/polyhomeb/mvp/p/activity/PropertyDetailActivityPresenter.java
+++ app/src/main/java/com/techjumper/polyhomeb/mvp/p/activity/PropertyDetailActivityPresenter.java
@@ -57,6 +57,7 @@ public class PropertyDetailActivityPresenter extends AppBaseActivityPresenter<Pr
             case 1:
                 bundle.putInt(Constant.PROPERTY_REPAIR_STATUS, mRepairStatus);
                 new AcHelper.Builder(getView()).extra(bundle).target(NewRepairActivity.class).start();
+                break;
             case 2:
                 bundle.putInt(Constant.PROPERTY_COMPLAIN_STATUS, mComplainStatus);
                 new AcHelper.Builder(getView()).extra(bundle).target(NewComplainActivity.class).start();
