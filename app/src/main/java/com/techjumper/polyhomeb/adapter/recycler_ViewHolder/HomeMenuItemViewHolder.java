a18b31e38f210773bc566e4a6cb47ba06c1b81d0
lixin <zhuxianlixin@gmail.com>
2016-11-14 22:19:00 +0800

    优化布局

--- app/src/main/java/com/techjumper/polyhomeb/adapter/recycler_ViewHolder/HomeMenuItemViewHolder.java
+++ app/src/main/java/com/techjumper/polyhomeb/adapter/recycler_ViewHolder/HomeMenuItemViewHolder.java
@@ -33,29 +33,5 @@ public class HomeMenuItemViewHolder extends BaseRecyclerViewHolder<HomeMenuItemD
         } else {
             getView(R.id.title_plus).setVisibility(View.GONE);
         }
-//        int layoutPosition = getLayoutPosition();
-//        setOnClickListener(R.id.root, v -> {
-//            startActivity(layoutPosition);
-//        });
     }
-
-
-//    private void startActivity(int position) {
-//        //item中还包括了divider,所以position的值有点不一样
-//        switch (position) {
-//            case 1:  //我的小区
-//                new AcHelper.Builder((Activity) getContext()).target(MyVillageFamilyActivity.class).start();
-//                break;
-//            case 3:  //智能家居设置
-//                break;
-//            case 5:  //信息中心
-//                new AcHelper.Builder((Activity) getContext()).target(MessageCenterActivity.class).start();
-//                break;
-//            case 9:  //我的积分
-//                break;
-//            case 13:  //设置
-//                new AcHelper.Builder((Activity) getContext()).target(SettingActivity.class).start();
-//                break;
-//        }
-//    }
 }
