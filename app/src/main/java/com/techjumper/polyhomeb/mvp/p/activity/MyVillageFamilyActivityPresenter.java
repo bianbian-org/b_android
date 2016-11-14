433ce43ffb143b0969e65f17acf8cf84a48eec8f
lixin <zhuxianlixin@gmail.com>
2016-11-14 22:51:14 +0800

    优化细节

--- app/src/main/java/com/techjumper/polyhomeb/mvp/p/activity/MyVillageFamilyActivityPresenter.java
+++ app/src/main/java/com/techjumper/polyhomeb/mvp/p/activity/MyVillageFamilyActivityPresenter.java
@@ -7,8 +7,10 @@ import com.techjumper.corelib.rx.tools.RxBus;
 import com.techjumper.corelib.rx.tools.RxUtils;
 import com.techjumper.corelib.utils.common.AcHelper;
 import com.techjumper.polyhomeb.Constant;
+import com.techjumper.polyhomeb.R;
 import com.techjumper.polyhomeb.adapter.databean.MyVillageFamilyBean;
 import com.techjumper.polyhomeb.entity.BluetoothLockDoorInfoEntity;
+import com.techjumper.polyhomeb.entity.QueryFamilyEntity;
 import com.techjumper.polyhomeb.entity.UserFamiliesAndVillagesEntity;
 import com.techjumper.polyhomeb.entity.event.BLEInfoChangedEvent;
 import com.techjumper.polyhomeb.entity.event.ChangeVillageIdRefreshEvent;
@@ -20,8 +22,10 @@ import com.techjumper.polyhomeb.user.UserManager;
 
 import java.util.List;
 
+import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
+import rx.functions.Func1;
 
 /**
  * * * * * * * * * * * * * * * * * * * * * * *
@@ -86,8 +90,7 @@ public class MyVillageFamilyActivityPresenter extends AppBaseActivityPresenter<M
     }
 
     private void onClickItem() {
-        getView().showLoading();
-        RxUtils.unsubscribeIfNotNull(mSubs2);
+//        RxUtils.unsubscribeIfNotNull(mSubs2);
         addSubscription(
                 mSubs2 = RxBus.INSTANCE.asObservable()
                         .subscribe(o -> {
@@ -112,10 +115,56 @@ public class MyVillageFamilyActivityPresenter extends AppBaseActivityPresenter<M
     }
 
     private void getBLEDoorInfo() {
+        getView().showLoading(false);
         RxUtils.unsubscribeIfNotNull(mSubs3);
+//        addSubscription(
+//                mSubs3 = mModel.getBLEDoorInfo()
+//                        .subscribe(new Observer<BluetoothLockDoorInfoEntity>() {
+//                            @Override
+//                            public void onCompleted() {
+//                                getView().dismissLoading();
+//                            }
+//
+//                            @Override
+//                            public void onError(Throwable e) {
+//                                getView().dismissLoading();
+//                                getView().showError(e);
+//                            }
+//
+//                            @Override
+//                            public void onNext(BluetoothLockDoorInfoEntity bluetoothLockDoorInfoEntity) {
+//                                if (!processNetworkResult(bluetoothLockDoorInfoEntity)) return;
+//                                if (bluetoothLockDoorInfoEntity != null
+//                                        && bluetoothLockDoorInfoEntity.getData() != null) {
+//                                    //切换家庭或者小区之后，发送消息给HomeFragment,刷新首页数据
+//                                    RxBus.INSTANCE.send(new BLEInfoChangedEvent());
+//                                    UserManager.INSTANCE.saveBLEInfo(bluetoothLockDoorInfoEntity);
+//                                }
+//                            }
+//                        }));
         addSubscription(
                 mSubs3 = mModel.getBLEDoorInfo()
-                        .subscribe(new Observer<BluetoothLockDoorInfoEntity>() {
+                        .flatMap(new Func1<BluetoothLockDoorInfoEntity, Observable<QueryFamilyEntity>>() {
+                            @Override
+                            public Observable<QueryFamilyEntity> call(BluetoothLockDoorInfoEntity bluetoothLockDoorInfoEntity) {
+                                if (!processNetworkResult(bluetoothLockDoorInfoEntity)) {
+                                    return Observable.error(new Exception(getView().getString(R.string.error_data)));
+                                }
+                                if (bluetoothLockDoorInfoEntity != null
+                                        && bluetoothLockDoorInfoEntity.getData() != null) {
+                                    //切换家庭或者小区之后，发送消息给HomeFragment,刷新首页数据
+                                    RxBus.INSTANCE.send(new BLEInfoChangedEvent());
+                                    UserManager.INSTANCE.saveBLEInfo(bluetoothLockDoorInfoEntity);
+                                } else {
+                                    return Observable.error(new Exception(getView().getString(R.string.error_data)));
+                                }
+                                if (UserManager.INSTANCE.isFamily()) {
+                                    return mModel.getCurrentFamilyAdminUserId();
+                                } else {
+                                    return Observable.empty();
+                                }
+                            }
+                        }).subscribe(new Observer<QueryFamilyEntity>() {
                             @Override
                             public void onCompleted() {
                                 getView().dismissLoading();
@@ -123,18 +172,19 @@ public class MyVillageFamilyActivityPresenter extends AppBaseActivityPresenter<M
 
                             @Override
                             public void onError(Throwable e) {
-                                getView().dismissLoading();
                                 getView().showError(e);
                             }
 
                             @Override
-                            public void onNext(BluetoothLockDoorInfoEntity bluetoothLockDoorInfoEntity) {
-                                if (!processNetworkResult(bluetoothLockDoorInfoEntity)) return;
-                                if (bluetoothLockDoorInfoEntity != null
-                                        && bluetoothLockDoorInfoEntity.getData() != null) {
-                                    //切换家庭或者小区之后，发送消息给HomeFragment,刷新首页数据
-                                    RxBus.INSTANCE.send(new BLEInfoChangedEvent());
-                                    UserManager.INSTANCE.saveBLEInfo(bluetoothLockDoorInfoEntity);
+                            public void onNext(QueryFamilyEntity queryFamilyEntity) {
+                                if (!processNetworkResult(queryFamilyEntity)) return;
+                                if (queryFamilyEntity != null && queryFamilyEntity.getData() != null) {
+                                    QueryFamilyEntity.DataEntity data = queryFamilyEntity.getData();
+                                    String manager_id = data.getManager_id();
+                                    UserManager.INSTANCE.saveUserInfo(UserManager.KEY_CURRENT_FAMILY_MANAGER_ID, manager_id);
+                                    UserManager.INSTANCE.notifyLoginOrLogoutEvent(true);
+                                } else {
+                                    getView().showHint(getView().getString(R.string.error_data));
                                 }
                             }
                         }));
