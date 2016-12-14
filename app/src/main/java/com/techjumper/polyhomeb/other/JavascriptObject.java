f730c50578d498625b461bbf261021e99339b0ad
lixin <zhuxianlixin@gmail.com>
2016-12-14 17:45:59 +0800

    JS点击跳转判断间隔时间800毫秒

--- app/src/main/java/com/techjumper/polyhomeb/other/JavascriptObject.java
+++ app/src/main/java/com/techjumper/polyhomeb/other/JavascriptObject.java
@@ -49,7 +49,7 @@ public class JavascriptObject {
         long time = System.currentTimeMillis();
         long timeD = time - lastClickTime;
         boolean flag = this.url.equalsIgnoreCase(url);
-        if (0 < timeD && timeD < 300 && flag) {
+        if (0 < timeD && timeD < 800 && flag) {
             this.url = url;
             return true;
         }
@@ -76,6 +76,7 @@ public class JavascriptObject {
                 JSJavaPageJumpEntity.ParamsBean params = jsJavaPageJumpEntity.getParams();
                 String url = params.getUrl();
                 if (!isFastDoubleClick(url)) {
+                    JLog.e("..................");
                     pageJump(url);
                 }
                 break;
